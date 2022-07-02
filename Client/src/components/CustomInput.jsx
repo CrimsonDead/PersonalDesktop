ffbb9ac5cd888cd1b2ebf0e7bb2d7b478825db90
Mikita Malashevich <mikita.malashevich@startmatter.com>
2022-07-02 17:40:10 +0300

    create custom input component

--- Client/src/components/CustomInput.jsx
+++ Client/src/components/CustomInput.jsx
@@ -1,20 +1,24 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { isEmpty } from "lodash";
 // import '../../images/'
 
 const CustomInput = ({placeHolder = "Password"}) => {
-    const ref = useRef();
-    const [text, setText] = useState('');
+    const [state, setState] = useState('');
     const onInputChange = (event) => {
         const {target: {value}} = event;
-        setText(value);
-        console.log(ref);
-        console.log(text, event, value);
-
+        setState(value);
     };
+
     return (
-        <input className="customInput" placeholder={placeHolder} text={text} onChange={console.log(1)} ref={ref} />
+        <>
+            <input
+                className="customInput"
+                placeholder={placeHolder}
+                text={state}
+                onChange={(event) => onInputChange(event)}
+            />
+        </>
     );
 };
 
