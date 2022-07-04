8fa764ee7d57f0fde21004a56889b2d399421b77
Llich <kir120056@gmail.com>
2022-07-04 11:47:57 +0300

    Create home component

---
+++ Client/src/components/HomePage/HomeBody.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeBody = () => {
+    return (
+        <div className="homePage">
+            <a className="homePageTime">10:48</a>
+            <a className="homePageDate">Friday, 20 April</a>
+        </div>
+    );
+};
+
+export default HomeBody;
+
+<div className="homePage">
+            <p className="homePageTime"><a>10:48</a></p>
+            <p className="homePageDate"><a>Friday, 20 April</a></p>
+        </div>
